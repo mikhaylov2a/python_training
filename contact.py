05aadb309eac2d35cfb8bb2f077aec6304a089cb
Aleksey <mikhailov2a@gmail.com>
2020-01-19 10:21:42 +0300

    added test add contact

---
+++ contact.py
@@ -0,0 +1,23 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, companyname, firstaddress, homephone,
+                  mobilephone, workphone, faxphone, firstemail, secondemail, thirdemail, homepage, secondaddress,
+                  phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.companyname = companyname
+        self.firstaddress = firstaddress
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.faxphone = faxphone
+        self.firstemail = firstemail
+        self.secondemail = secondemail
+        self.thirdemail = thirdemail
+        self.homepage = homepage
+        self.secondaddress = secondaddress
+        self.phone2 = phone2
+        self.notes = notes
